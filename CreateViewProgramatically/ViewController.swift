24ea70627afc0fc01abc530efa028583574163e3
Ravi Parsa <raviparsa@a1-dh-d042.local>
2018-03-13 15:01:47 +0530

    Initial Commit

---
+++ CreateViewProgramatically/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CreateViewProgramatically
+//
+//  Created by Ravi Parsa on 13/03/18.
+//  Copyright © 2018 Ravi Parsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
